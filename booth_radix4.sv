58a8a9f3aae9945e0f527aedf3bbee2e2eebac91
Benjamin Mordaunt <crawford.benjamin15@gmail.com>
2023-07-16 14:15:25 +0100

    Initial commit (2)
    Signed-off-by: Benjamin Mordaunt <crawford.benjamin15@gmail.com>

--- booth_radix4.sv
+++ booth_radix4.sv
@@ -20,14 +20,30 @@ reg [4:0] M_neg2;
 reg [9:0] partial_product;
 reg [1:0] count;
 
+logic [4:0] addend;
+
 assign M_ext  = {M[3], M[3:0]};
 assign M_neg  = -M_ext;
 assign M_2    = M_ext << 1;
 assign M_neg2 = M_neg << 1;
 
+// Partial product multiplier selection
+always_comb begin
+  case (partial_product[2:0])
+    3'b000         : addend = 5'b0;
+    3'b001, 3'b010 : addend = M_ext;
+    3'b011         : addend = M_2;
+    3'b100         : addend = M_neg2;
+    3'b101, 3'b110 : addend = M_neg;
+    3'b111         : addend = 5'b0;
+    default        : addend = 5'bx;
+  endcase
+end
+
 always_ff @(posedge clk, negedge resetn) begin
   if (resetn == 0) begin
     partial_product <= 10'b0;
+    product <= 9'b0;
     count <= 2'b0;
     valid <= 0;
   end else begin
@@ -36,16 +52,8 @@ always_ff @(posedge clk, negedge resetn) begin
       count <= count + 1;
     end
     else begin
-      if (count < 2) begin
-        case (partial_product[2:0])
-          3'b000         : partial_product <= (partial_product >>> 2);
-          3'b001, 3'b010 : partial_product <= ({(partial_product[9:5] + M_ext), partial_product[4:0]} >>> 2);
-          3'b011         : partial_product <= ({(partial_product[9:5] + M_2), partial_product[4:0]} >>> 2);
-          3'b100         : partial_product <= ({(partial_product[9:5] + M_neg2), partial_product[4:0]} >>> 2);
-          3'b101, 3'b110 : partial_product <= ({(partial_product[9:5] + M_neg), partial_product[4:0]} >>> 2); 
-          3'b111         : partial_product <= (partial_product >>> 2);
-          default        : partial_product <= 10'bx;
-        endcase
+      if (count <= 2) begin
+        partial_product <= ({(partial_product[9:5] + addend), partial_product[4:0]} >>> 2);
         count <= count + 1;
       end
       else
